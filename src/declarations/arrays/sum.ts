ab5f89ef97eb05115ac2a89e8c38fa9b937d5a76
nvd05 <nvd05@bk.ru>
2025-10-09 16:50:06 +0500

    Update sum.ts

--- src/declarations/arrays/sum.ts
+++ src/declarations/arrays/sum.ts
@@ -3,12 +3,18 @@ import './sum.d';
 
 if (typeof Array.prototype.sum === 'undefined')
 {
-	Array.prototype.sum = function (this, fractionDigits?: number): number
-	{
-		const response = this.reduce((accumulator, current) => accumulator + current, 0);
+	Object.defineProperty(Array.prototype, 'sum', {
+		configurable: true,
+		enumerable: false,
+		writable: true,
 
-		return fractionDigits
-			? response.toFixedNumber(fractionDigits)
-			: response;
-	};
+		value: function (this: number[], fractionDigits?: number): number
+		{
+			const response = this.reduce((accumulator, current) => accumulator + current, 0);
+
+			return fractionDigits
+				? response.toFixedNumber(fractionDigits)
+				: response;
+		},
+	});
 }
