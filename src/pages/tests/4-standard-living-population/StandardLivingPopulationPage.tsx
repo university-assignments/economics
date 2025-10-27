f455d8beae2362298e9952abb3dd91e2b175e26f
nvd05 <nvd05@bk.ru>
2025-10-27 22:59:27 +0500

    Update StandardLivingPopulationPage.tsx

--- src/pages/tests/4-standard-living-population/StandardLivingPopulationPage.tsx
+++ src/pages/tests/4-standard-living-population/StandardLivingPopulationPage.tsx
@@ -1,7 +1,9 @@
-import { Stack, TextField, Typography } from '@mui/material';
+import { Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 import { MathJax, MathJaxContext } from 'better-react-mathjax';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
+import { StyledTableCell } from 'src/components/table/StyledTableCell';
+import { StyledTableRow } from 'src/components/table/StyledTableRow';
 
 
 const ln = (val: number) => Math.log(val);
@@ -102,16 +104,59 @@ export default function StandardLivingPopulationPage ()
 					</Stack>
 				</Stack>
 
+				<TableContainer>
+					<Table>
+						<TableHead>
+							<TableRow>
+								<TableCell></TableCell>
+								<TableCell>Ед. Изм.</TableCell>
+								<TableCell>P min</TableCell>
+								<TableCell>P max</TableCell>
+							</TableRow>
+						</TableHead>
+
+						<TableBody>
+							<StyledTableRow>
+								<StyledTableCell>P1</StyledTableCell>
+								<StyledTableCell>лет</StyledTableCell>
+								<StyledTableCell>25</StyledTableCell>
+								<StyledTableCell>85</StyledTableCell>
+							</StyledTableRow>
+
+							<StyledTableRow>
+								<StyledTableCell>P2</StyledTableCell>
+								<StyledTableCell>%</StyledTableCell>
+								<StyledTableCell>0</StyledTableCell>
+								<StyledTableCell>100</StyledTableCell>
+							</StyledTableRow>
+
+							<StyledTableRow>
+								<StyledTableCell>P3</StyledTableCell>
+								<StyledTableCell>%</StyledTableCell>
+								<StyledTableCell>0</StyledTableCell>
+								<StyledTableCell>100</StyledTableCell>
+							</StyledTableRow>
+
+							<StyledTableRow>
+								<StyledTableCell>P4</StyledTableCell>
+								<StyledTableCell>$</StyledTableCell>
+								<StyledTableCell>100</StyledTableCell>
+								<StyledTableCell>40000</StyledTableCell>
+							</StyledTableRow>
+						</TableBody>
+					</Table>
+				</TableContainer>
+
 				<Stack direction='row' spacing={2}>
 					<Typography variant='h6'>1)</Typography>
 
 					<Stack spacing={1} mt={2}>
-						<MathJax>{ `\\(i(P_1) = \\frac{${p1}-25}{85-25} = \\frac{${p1 - 25}}{60} = ${ip1}\\)` }</MathJax>
-						<MathJax>{ `\\(i(P_2) = \\frac{${p2}-0}{100-0} = ${ip2}\\)` }</MathJax>
-						<MathJax>{ `\\(i(P_3) = \\frac{${p3}-0}{100-0} = ${ip3}\\)` }</MathJax>
-						<MathJax>{ `\\(i(обр) = \\frac{2 * ${ip2} + ${ip3}}{3} = ${iobr}\\)`}</MathJax>
-						<MathJax>{ `\\(i(P_4) = \\frac{\\ln ${p4} - \\ln 100}{\\ln 40000 - \\ln 100} = ${ip4}\\)` }</MathJax>
-						<MathJax>{ `\\(ИРИП = \\frac{${ip1} + ${iobr} + ${ip4}}{3} = \\boxed{${irip}}\\)` }</MathJax>
+						<MathJax>{ `\\(i(P_1) = \\frac{P1(Ф) - P1(min)}{P1(max) - P1(min)} = \\frac{${p1}-25}{85-25} = \\frac{${p1 - 25}}{60} = ${ip1}\\)` }</MathJax>
+						<MathJax>{ `\\(i(P_2) = \\frac{P2(Ф) - P2(min)}{P2(max) - P2(min)} = \\frac{${p2}-0}{100-0} = ${ip2}\\)` }</MathJax>
+						<MathJax>{ `\\(i(P_3) = \\frac{P3(Ф) - P3(min)}{P3(max) - P3(min)} = \\frac{${p3}-0}{100-0} = ${ip3}\\)` }</MathJax>
+						<MathJax>{ `\\(i(обр) = \\frac{2 * i(P2) + i(P3)}{3} = \\frac{2 * ${ip2} + ${ip3}}{3} = ${iobr}\\)`}</MathJax>
+						<MathJax>{ `\\(i(P_4) = \\frac{P4(Ф) - P4(min)}{P4(max) - P4(min)} = \\frac{\\ln ${p4} - \\ln 100}{\\ln 40000 - \\ln 100} = ${ip4}\\)` }</MathJax>
+						<MathJax>{ `\\(ИРИП = \\frac{i(P1) + i(обр) + i(p4)}{3} = \\frac{${ip1} + ${iobr} + ${ip4}}{3} = \\boxed{${irip}}\\)` }</MathJax>
 					</Stack>
 				</Stack>
 
@@ -119,15 +164,15 @@ export default function StandardLivingPopulationPage ()
 					<Typography variant='h6'>2)</Typography>
 
 					<Stack spacing={1} mt={2}>
-						<MathJax>{ `\\(H_3 = \\frac{${h31} + ${h32} + ${h33}}{3} = ${h3}\\)` }</MathJax>
-						<MathJax>{ `\\(ИНН(1) = \\sqrt[3]{\\frac{${h1}^3 + ${h2}^3 + ${h3}^3}{3}} = \\boxed{${inn1}\\%}\\)` }</MathJax>
+						<MathJax>{ `\\(H_3 = \\frac{H_{31} + H_{32} + H_{33}}{3} = \\frac{${h31} + ${h32} + ${h33}}{3} = ${h3}\\)` }</MathJax>
+						<MathJax>{ `\\(ИНН(1) = (\\frac{M_1^3 + M_2^3 + M_3^3}{3})^{\\frac{1}{3}} = \\sqrt[3]{\\frac{${h1}^3 + ${h2}^3 + ${h3}^3}{3}} = \\boxed{${inn1}\\%}\\)` }</MathJax>
 					</Stack>
 				</Stack>
 
 				<Stack direction='row' spacing={2}>
 					<Typography variant='h6'>3)</Typography>
 
-					<MathJax>{ `\\(ИНН(2) = \\sqrt[3]{\\frac{${n1}^3 + ${n2}^3 + ${n3}^3 + ${n4}^3}{4}} = \\boxed{${inn2}\\%}\\)` }</MathJax>
+					<MathJax>{ `\\(ИНН(2) = (\\frac{N_1^3 + N_2^3 + N_3^3 + N_4^3}{4})^{\\frac{1}{3}} = \\sqrt[3]{\\frac{${n1}^3 + ${n2}^3 + ${n3}^3 + ${n4}^3}{4}} = \\boxed{${inn2}\\%}\\)` }</MathJax>
 				</Stack>
 			</Stack>
 		</MathJaxContext>
